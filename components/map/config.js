739b329b8b14f5a358766b45080c6fe5df18766d
Kata Martin <katasm@gmail.com>
2021-07-22 15:12:50 -0700

    Use tileset from Google Storage

--- components/map/config.js
+++ components/map/config.js
@@ -1,6 +1,6 @@
 const prefix =
-  // 'https://carbonplan.blob.core.windows.net/carbonplan-forests/risks/tiles'
-  'http://localhost:8080'
+  'https://storage.googleapis.com/carbonplan-research/macroalgae/data/tiles'
+// 'http://localhost:8080'
 
 export default {
   basemap: `${prefix}/basemap`,
