95159055fc37d893f62aae3ac11073126cc6d807
Kata Martin <katasm@gmail.com>
2021-08-04 09:59:39 -0700

    Set up next config to support local linking

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const slug = require('rehype-slug')
+const path = require('path')
 
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
@@ -9,4 +10,22 @@ const withMDX = require('@next/mdx')({
 
 module.exports = withMDX({
   pageExtensions: ['jsx', 'js', 'md', 'mdx'],
+  webpack: (config, options) => {
+    if (options.isServer) {
+      config.externals = ['react', 'theme-ui', ...config.externals]
+    }
+    config.resolve.alias['react'] = path.resolve(
+      __dirname,
+      '.',
+      'node_modules',
+      'react'
+    )
+    config.resolve.alias['theme-ui'] = path.resolve(
+      __dirname,
+      '.',
+      'node_modules',
+      'theme-ui'
+    )
+    return config
+  },
 })
