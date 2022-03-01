9d2cb6e0b34082972a49dbda4c434aa1e8c7f833
Kata Martin <katasm@gmail.com>
2022-03-01 12:46:02 -0800

    Use "big number" average display

--- components/statistics/average-display.js
+++ components/statistics/average-display.js
@@ -1,37 +1,41 @@
-import { Box } from 'theme-ui'
-import { Badge, Row, Column } from '@carbonplan/components'
+import { Box, Flex } from 'theme-ui'
 import { formatValue } from '../utils'
 
-const AverageDisplay = ({ value, label, units }) => {
+const AverageDisplay = ({ value, label, units, color = 'teal' }) => {
   return (
-    <Row columns={3} sx={{ mb: ['18px'] }}>
-      <Column start={1} width={2}>
+    <Box>
+      <Box
+        sx={{
+          fontFamily: 'faux',
+          letterSpacing: 'smallcaps',
+          fontSize: [2, 2, 2, 3],
+        }}
+      >
+        {label}
+      </Box>
+      <Flex sx={{ gap: 2, alignItems: 'baseline' }}>
+        <Box
+          sx={{
+            fontFamily: 'mono',
+            letterSpacing: 'mono',
+            fontSize: [3, 3, 4, 5],
+            color,
+          }}
+        >
+          {formatValue(value)}
+        </Box>
         <Box
           sx={{
             fontFamily: 'faux',
-            letterSpacing: 'smallcaps',
-            fontSize: [2, 2, 2, 3],
+            letterSpacing: 'faux',
+            fontSize: [0, 0, 0, 1],
+            color: 'secondary',
           }}
         >
-          {label}
-          <Box
-            sx={{
-              fontFamily: 'faux',
-              letterSpacing: 'faux',
-              fontSize: [0, 0, 0, 1],
-              color: 'secondary',
-            }}
-          >
-            {units}
-          </Box>
+          {units}
         </Box>
-      </Column>
-      <Column start={3} width={1}>
-        <Box sx={{ mt: ['4px'] }}>
-          <Badge>{Number.isNaN(value) ? 'n/a' : formatValue(value)}</Badge>
-        </Box>
-      </Column>
-    </Row>
+      </Flex>
+    </Box>
   )
 }
 
