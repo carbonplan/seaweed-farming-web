f45591c79eae0df1516dd224d723017a1f4f53ee
freeman-lab <the.freeman.lab@gmail.com>
2022-03-15 21:17:11 -0700

    add asset prefix

--- next.config.js
+++ next.config.js
@@ -1,22 +1,8 @@
 const path = require('path')
 
+const isDev =
+  process.env.VERCEL_ENV === 'preview' || process.env.NODE_ENV === 'development'
+
 module.exports = {
-  webpack: (config, options) => {
-    if (options.isServer) {
-      config.externals = ['react', 'theme-ui', ...config.externals]
-    }
-    config.resolve.alias['react'] = path.resolve(
-      __dirname,
-      '.',
-      'node_modules',
-      'react'
-    )
-    config.resolve.alias['theme-ui'] = path.resolve(
-      __dirname,
-      '.',
-      'node_modules',
-      'theme-ui'
-    )
-    return config
-  },
+  assetPrefix: isDev ? '' : 'https://seaweed-farming.carbonplan.org',
 }
